81a715744c9271c849f62df9facac7076a5b0e39
Jamie McInerney <jamie@flypay.co.uk>
2016-11-29 16:05:49 +0000

    feat: Using the share operator
    This commit demonstrates the use of the share operator to reduce our requests to the api

--- src/app/wizard/wizard.component.ts
+++ src/app/wizard/wizard.component.ts
@@ -1,33 +1,22 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WizardService, IWizardStep} from './wizard.service';
-import {Observable, Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-wizard',
   templateUrl: 'wizard.component.html'
 })
-export class WizardComponent implements OnInit, OnDestroy {
-  private subscriptions: Array<Subscription> = [];
+export class WizardComponent implements OnInit {
   wizardNextStep: Observable<IWizardStep>;
 
   constructor(private wizardService: WizardService) { }
 
   ngOnInit() {
-    this.wizardNextStep = this.wizardService.getNextWizardStep();
+    this.wizardNextStep = this.wizardService.getNextWizardStep().share();
   }
 
-  ngOnDestroy() {
-    for(let i=0; i < this.subscriptions.length; i++){
-      this.subscriptions[i].unsubscribe();
-    }
-  }
-
-  /**
-   * A simple method that subscribes to the wizardNextStep.
-   */
   navigate(): void {
-    let sub = this.wizardNextStep.subscribe((response) => console.warn('navigate ==> ', response));
-    this.subscriptions.push(sub);
+    this.wizardNextStep.subscribe((response) => console.warn('Navigate User To: ', response.link));
   }
 
 }
